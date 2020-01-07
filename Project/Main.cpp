e9a07de78d33d02fde04baa1e54cd590e4e8c086
erannya <sylwiamalmon@op.pl>
2020-01-07 22:59:43 +0100

    I used while loop to "AskToPlayAgain" to play the game again or not .

--- Project/Main.cpp
+++ Project/Main.cpp
@@ -1,34 +1,31 @@
-﻿// Project.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 
 using namespace std;
 
 void PrintIntro();
-string GetGuessAndPrintBack();
+void PlayGame();
+string GetGuess();
+bool AskToPlayAgain();
 
 // the entry point for our application
 int main()
 {
-    PrintIntro();
+    bool bPlayAgain = false;
+    do {
+        PrintIntro();
+        PlayGame();
+        bPlayAgain = AskToPlayAgain();
+    } while (bPlayAgain);
 
-    // loop for the number of turns asking for guesses
-    constexpr int NUMBER_OF_TURNS = 5;
-    for (int count = 1; count <= NUMBER_OF_TURNS; count++)
-    {
-    GetGuessAndPrintBack();
-    cout << endl;
-    }
-    
-    return 0;
+    return 0; //exit the application
 }
+
 
 // introduce the game
 void PrintIntro() 
 {
-    constexpr int WORLD_LENGTH = 5;
+    constexpr int WORLD_LENGTH = 9;
     cout << "Welcome to Bulls and Cows, a fun word game!\n";
     cout << "Can you guess the " << WORLD_LENGTH;
     cout << " letter isogram I'm thinking of?\n";
@@ -36,24 +33,33 @@ void PrintIntro()
     return;
 }
 
-// get a guess from the player
-string GetGuessAndPrintBack() 
+
+void PlayGame()
 {
+    // loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++) {
+		string Guess = GetGuess();
+		cout << "Your guess was: " << Guess << endl;
+		cout << endl;
+	}
+
+}
+
+
+string GetGuess()
+{
+    // get a guess from the player
     cout << "Enter your guess: ";
     string Guess = "";
     getline(cin, Guess);
-
-    // print the guess back
-    cout << "Your guess was: " << Guess << endl;
     return Guess;
 }
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
 
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
+bool AskToPlayAgain()
+{
+    cout << "Do you want to play again? (yes/no)";
+    string Response = "";
+    getline(cin, Response);
+    return (Response[0] == 'y') || (Response[0] == 'Y');
+}
